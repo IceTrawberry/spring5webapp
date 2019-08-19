daf814ae1070ed2746529523eec601d0c3941dda
arya <zyu252@wisc.edu>
2019-08-19 18:22:19 -0500

    Added Publisher Entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,12 +15,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap( AuthorRepository authorRepository, BookRepository bookRepository ) {
+    public DevBootstrap( AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository ) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
-
 
     @Override
     public void onApplicationEvent( ContextRefreshedEvent contextRefreshedEvent ) {
@@ -27,17 +30,22 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData() {
 
+        Publisher publisher=new Publisher();
+        publisher.setName("Harper Collins");
+        publisher.setAddress("4817 Sheboygan Ave");
+        publisherRepository.save(publisher);
         //Shaw
         Author shaw = new Author("Shaw", "Wang");
-        Book book1 = new Book("I Am Not Kind", "1112", "Harper Collins");
+        Book book1 = new Book("I Am Not Kind", "1112", publisher);
         shaw.getBooks().add(book1);
         book1.getAuthors().add(shaw);
         authorRepository.save(shaw);
         bookRepository.save(book1);
 
+
         //Dorothy
         Author dorothy = new Author("Dorothy", "Dong");
-        Book book2 = new Book("I Am Not Cute", "0625", "Harper Collins");
+        Book book2 = new Book("I Am Not Cute", "0625", publisher);
         dorothy.getBooks().add(book2);
         book2.getAuthors().add(dorothy);
         authorRepository.save(dorothy);
